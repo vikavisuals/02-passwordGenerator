f4b22ce569b36d0b0fcfbfe9dd6a14fc042fe759
vkaszuba <codeword.vika@gmail.com>
2020-01-14 15:32:44 -0800

    Lower and Upper case loops are printing length of their loop

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate"); //Grabbing everything with the Generate ID
+// Assigning variable to id="generate"
+const $generateBtn = document.querySelector("#generate");
+// Upon click, this runs writePassword function
+$generateBtn.addEventListener("click", writePassword);
 
 
-// Write password to the #password input
+// Write password to the id=password input
 function writePassword() {
 
+  
   let password = generatePassword();
   let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+
 
   // copyBtn.removeAttribute("disabled");
   // copyBtn.focus();
@@ -20,36 +25,73 @@ function generatePassword() {
   const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   const numbers = "1234567890";
   const symbols = "~`!@#$%^&*()_-+={}[]";
+  let randomPassword = "";
+  let randomLowerCase = "";
+  let randomUpperCase = "";
 
-  const userAmount = parseInt(document.getElementById("userAmountBox").value);
+  // Makes the user input turn into a number, versus a string
+  const passLength = parseInt(document.getElementById("passLength").value);
 
-    if (document.getElementById("lowerCaseBox").checked) {
-      for (i = 0; i < userAmount; i++) {
-        let randomNum = Math.floor(Math.random() * lowerCase.length);
-        console.log (lowerCase[randomNum]);
-      }
-    } else {
-      console.log("not checked");
+  if (document.getElementById("lowerBox").checked) {
+    for (i = 0; i < passLength; i++) {
+      randomLowerCase += lowerCase.charAt(Math.floor(Math.random() * lowerCase.length));
+      // lowerCase.length is a number of 26
+
     }
-  
-    if (document.getElementById("upperCaseBox").checked) {
-      for (i = 0; i < userAmount; i++) {
-        let randomNum = Math.floor(Math.random() * upperCase.length);
-        console.log (upperCase[randomNum]);
-      }
-    } else {
-      console.log("not checked");
-    }
+  } else {
+    console.log("not checked");
+  }
 
+  if (document.getElementById("upperBox").checked) {
+    for (i = 0; i < passLength; i++) {
+      randomUpperCase += upperCase.charAt(Math.floor(Math.random() * upperCase.length));
+
+    }
+  } else {
+    console.log("not checked");
+ }
+ console.log(randomUpperCase);
+ console.log(randomLowerCase);
 }
 
 
 
-function copyToClipboard() {
-  // BONUS 
-}
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
+
+
+// function copyToClipboard() {
+//   // BONUS 
+// }
+
+
 
 // BONUS EVENT LISTENER
+
+
+// query or getelement works here, it's preference
+// const $shorterPassword = document.getElementById("shorterPassword");
+// const $longerPassword = document.getElementById("longerPassword");
+// const $passLength = parseInt(document.getElementById("passLength").value);
+
+// let passLength = 10;
+
+// $longerPassword.addEventListener("click", function () {
+//   if (passLength < 128) {
+//     passLength++;
+//         $passLength.value = passLength;
+//     } else {  
+//         $passLength.value = passLength;
+//     }
+// });
+
+// $shorterPassword.addEventListener("click", function () {
+//   if (passLength < 9) {
+//       $passLength.value = passLength;
+//   } else {
+//       passLength--;
+//       $passLength.value = passLength;
+//   }
+//   console.log(passLength);
+// });
