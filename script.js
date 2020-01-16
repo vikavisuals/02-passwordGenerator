a10d9ba4c7af6a5bbba9819f4d64be11a433484d
vkaszuba <codeword.vika@gmail.com>
2020-01-15 18:27:57 -0800

    Adding all options as individual checks, this works. Multiple selections aren't set up yet

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ $generateBtn.addEventListener("click", writePassword);
 // Write password to the id=password input
 function writePassword() {
 
+
   let password = generatePassword();
   let passwordText = document.querySelector("#password");
 
@@ -22,95 +23,66 @@ function generatePassword() {
   const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   const numbers = "1234567890";
   const symbols = "~`!@#$%^&*()_-+={}[]";
-
-  // Buffer variables to fill with random password elements
+  
   let randomPassword = "";
-  let randomLowerCase = "";
-  let randomUpperCase = "";
-  let randomNumbers = "";
-  let randomSymbols = "";
-  let multiRandom = "";
   let finalPassword = "";
 
 
-// Makes the user input turn into a number, versus a string
+  // Makes the user input turn into a number, versus a string
   const passLength = parseInt(document.getElementById("passLength").value);
   const $lowerChecked = (document.getElementById("lowerBox").checked);
   const $upperChecked = (document.getElementById("upperBox").checked);
   const $numbersChecked = (document.getElementById("numbersBox").checked);
   const $symbolsChecked = (document.getElementById("symbolsBox").checked);
 
-
-// Are both upper and lowercase checkboxes activated
-  if ($lowerChecked && $upperChecked && $numbersChecked) {
-    randomPassword = randomLowerCase + randomUpperCase + randomNumbers;
-
+  // Is lowercase checkbox activated
+  if ($lowerChecked) {
     for (i = 0; i < passLength; i++) {
-      multiRandom += randomPassword.charAt(Math.floor(Math.random() * randomPassword.length));
-      finalPassword = multiRandom;
-      console.log(finalPassword);
+      randomPassword += lowerCase.charAt(Math.floor(Math.random() * lowerCase.length));
+      finalPassword = randomPassword;
     }
-
-// Lowercase only active
-  } else if ($lowerChecked) {
-    for (i = 0; i < passLength; i++) {
-      randomLowerCase += lowerCase.charAt(Math.floor(Math.random() * lowerCase.length));
-      finalPassword = randomLowerCase;
-    }
-
-// Uppercase only active
-  } else if ($upperChecked) {
-    for (i = 0; i < passLength; i++) {
-      randomUpperCase += upperCase.charAt(Math.floor(Math.random() * upperCase.length));
-      finalPassword = randomUpperCase;
-    }
-
-// Numbers only active
-   } else if ($numbersChecked) {
-      for (i = 0; i < passLength; i++) {
-        randomNumbers += numbers.charAt(Math.floor(Math.random() * numbers.length));
-        finalPassword = randomNumbers;
-      }
-
-      // Symbols only active
-   } else if ($symbolsChecked) {
-    for (i = 0; i < passLength; i++) {
-      randomSymbols += symbols.charAt(Math.floor(Math.random() * symbols.length));
-      finalPassword = randomSymbols;
-    }
-
-// Lower and Upper active
-  } else if ($lowerChecked && $upperChecked) {
-    randomPassword = randomLowerCase + randomUpperCase;
-
-    for (i = 0; i < passLength; i++) {
-      multiRandom += randomPassword.charAt(Math.floor(Math.random() * randomPassword.length));
-      finalPassword = multiRandom;
-    }
-
-// Upper and numbers active
-  } else if ($upperChecked && $numbersChecked) {
-    randomPassword = randomUpperCase + randomNumbers;
-
-    for (i = 0; i < passLength; i++) {
-      multiRandom += randomPassword.charAt(Math.floor(Math.random() * randomPassword.length));
-      finalPassword = multiRandom;
-    }
-
-// Lower and numbers active
-  } else if ($lowerChecked && $numbersChecked) {
-    randomPassword = randomLowerCase + randomNumbers;
-
-    for (i = 0; i < passLength; i++) {
-      multiRandom += randomPassword.charAt(Math.floor(Math.random() * randomPassword.length));
-      finalPassword = multiRandom;
-    }
-
-  } else {
-    return "No selection made";
   }
 
+  // Is uppercase checkbox activated
+  if ($upperChecked) {
+    for (i = 0; i < passLength; i++) {
+      randomPassword += upperCase.charAt(Math.floor(Math.random() * upperCase.length));
+      finalPassword = randomPassword;
+    }
+  }
 
+ // Is numbers checkbox activated
+ if ($numbersChecked) {
+  for (i = 0; i < passLength; i++) {
+    randomPassword += numbers.charAt(Math.floor(Math.random() * numbers.length));
+    finalPassword = randomPassword;
+  }
+}
+
+ // Is uppercase checkbox activated
+ if ($symbolsChecked) {
+  for (i = 0; i < passLength; i++) {
+    randomPassword += symbols.charAt(Math.floor(Math.random() * symbols.length));
+    finalPassword = randomPassword;
+  }
+}
+
+
+  // // Are both upper and lowercase checkboxes activated
+  // if (($lowerChecked) && ($upperChecked)) {
+  //   randomPassword = randomLowerCase + randomUpperCase;
+  //   for (i = 0; i < passLength; i++) {
+  //     multiRandom += randomPassword.charAt(Math.floor(Math.random() * randomPassword.length));
+  //     finalPassword = multiRandom;
+  //   }
+  // } else {
+  //   console.log("Both are not checked");
+  // }
+
+
+
+  // console.log(randomPassword);
+  console.log(finalPassword);
   return finalPassword;
 
 }
